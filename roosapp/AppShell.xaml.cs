e03d53b2df3926a81760fa45fe759d8b0ecf4268
Ruben Zwinkels <rubenzwinkels05@gmail.com>
2025-08-11 15:01:41 +0200

    #5 [feat] maui hello world

---
+++ roosapp/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace roosapp;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ roosapp/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "roosapp")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "roosapp.Pages")]
