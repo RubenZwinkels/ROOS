a52c0b6e632632e168b2fe1674d7ba9be9ddcc80
Ruben Zwinkels <rubenzwinkels05@gmail.com>
2025-08-13 13:38:07 +0200

    #10 [feat] context toevoegen bij builden

--- roosapp/MauiProgram.cs
+++ roosapp/MauiProgram.cs
@@ -1,24 +1,29 @@
 ﻿using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+using roosapp.data;
 
 namespace roosapp;
 
 public static class MauiProgram
 {
-	public static MauiApp CreateMauiApp()
-	{
-		var builder = MauiApp.CreateBuilder();
-		builder
-			.UseMauiApp<App>()
-			.ConfigureFonts(fonts =>
-			{
-				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-			});
+    public static MauiApp CreateMauiApp()
+    {
+        var builder = MauiApp.CreateBuilder();
+
+        builder
+            .UseMauiApp<App>()
+            .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+            });
 
 #if DEBUG
-		builder.Logging.AddDebug();
+        builder.Logging.AddDebug();
 #endif
+        builder.Services.AddDbContext<DataContext>(options =>
+            options.UseSqlite($"Data Source={Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "dataContext.db")}"));
 
-		return builder.Build();
-	}
+        return builder.Build();
+    }
 }
